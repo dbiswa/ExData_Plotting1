e431958a5a953fac8a7c9830d18a9054770770b8
Debasmita Biswal <debasmitabiswal@Debasmitas-MBP.hitronhub.home>
2017-12-23 15:52:02 -0700

    R code to generate Plot1.png, Plot2.png, Plot3.png and Plot4.png

---
+++ plot3.R
@@ -0,0 +1,27 @@
+# Download data set, household_power_consumption.txt.
+# Use awk in command line (in the directory containing household_power_consumption.txt) to extract the header, and data corresponding to dates 2007-02-01 and 2007-02-02 from household_power_consumption.txt data set
+# awk -F";" 'FNR == 1 || $1 ~ /^1\/2\/2007/ || $1 ~ /^2\/2\/2007/' household_power_consumption.txt > subset.txt
+# The above command thus, created a suitable subset (subset.txt) from the original data set.
+# Assuming the working directory contains subset.txt, commands below can be used to generated required plots. 
+# use read.csv2 to read semicolon (;) delimited file, subset.txt
+hpc <- read.csv2(file="subset.txt", header = TRUE, stringsAsFactors = FALSE)
+# combined Date and Time to a single datetime object
+hpc$Date  <- with(hpc, as.POSIXct(paste(as.Date(Date, format="%d/%m/%Y"), Time)))
+# remove Time column from the data frame hpc
+hpc <- hpc [, -2]
+# load library magrittr
+library(magrittr)
+# convert the character classes to numeric
+hpc[, c(2:8)] %<>% lapply(function(x) as.numeric(x))
+
+# Command to generate Plot3.png
+# Open a png plot device to create Plot3.png with a width of 480 pixels and a height of 480 pixels
+png(file = "Plot3.png", width = 480, height = 480, units = "px")
+# use par command to set the number of rows and columns with mfrow, background (transparent is default), and margin
+par(mfrow = c(1,1), bg = "transparent", mar = c(4,4,2,1))
+# command to generate plot
+with(hpc, plot(Date, Sub_metering_1, type = "l", ylab = "Energy sub metering",xlab = "", col = "black"))
+with(hpc, lines(Date, Sub_metering_2, col="red"))
+with(hpc, lines(Date, Sub_metering_3, col="blue"))
+legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,32 @@
+# Download data set, household_power_consumption.txt.
+# Use awk in command line (in the directory containing household_power_consumption.txt) to extract the header, and data corresponding to dates 2007-02-01 and 2007-02-02 from household_power_consumption.txt data set
+# awk -F";" 'FNR == 1 || $1 ~ /^1\/2\/2007/ || $1 ~ /^2\/2\/2007/' household_power_consumption.txt > subset.txt
+# The above command thus, created a suitable subset (subset.txt) from the original data set.
+# Assuming the working directory contains subset.txt, commands below can be used to generated required plots. 
+# use read.csv2 to read semicolon (;) delimited file, subset.txt
+hpc <- read.csv2(file="subset.txt", header = TRUE, stringsAsFactors = FALSE)
+# combined Date and Time to a single datetime object
+hpc$Date  <- with(hpc, as.POSIXct(paste(as.Date(Date, format="%d/%m/%Y"), Time)))
+# remove Time column from the data frame hpc
+hpc <- hpc [, -2]
+# load library magrittr
+library(magrittr)
+# convert the character classes to numeric
+hpc[, c(2:8)] %<>% lapply(function(x) as.numeric(x))
+
+# Command to generate Plot4.png
+# Open a png plot device to create Plot4.png with a width of 480 pixels and a height of 480 pixels
+png(file = "Plot4.png", width = 480, height = 480, units = "px")
+# use par command to set the number of rows and columns with mfrow, background (transparent is default), and margin
+par(mfrow = c(2,2), bg = "white", mar = c(5,4,2,1))
+# command to generate four plots
+with(hpc, {
+  plot(Date, Global_active_power, type = "l", ylab = "Global Active Power",xlab = "", col = "black")
+  plot(Date, Voltage, type = "l", ylab = "Voltage", xlab = "datetime", col = "black")
+  plot(Date, Sub_metering_1, type = "l", ylab = "Energy sub metering",xlab = "", col = "black")  
+  lines(Date, Sub_metering_2, col="red")
+  lines(Date, Sub_metering_3, col="blue") 
+  legend("topright", bty = "n", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Date, Global_reactive_power, type = "l", xlab = "datetime", col = "black")
+})  
+dev.off()
